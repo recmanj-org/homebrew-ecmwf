75327343e377f82d468bd49cc689f092bc1eb5d7
Jakub Recman <jakub@oxidian.com>
2026-03-11 13:03:43 +0100

    Fix formula syntax

--- Formula/ecmwf-toolbox.rb
+++ Formula/ecmwf-toolbox.rb
@@ -1,10 +1,8 @@
 class TokenAuthGitDownloadStrategy < GitDownloadStrategy
   def initialize(url, name, version, **meta)
     token = ENV["ECMWF_TOOLBOX_TOKEN"]
-    if token
-      url = url.sub("https://github.com/", "https://x-access-token:#{token}@github.com/")
-    end
-    super(url, name, version, **meta)
+    url = url.sub("https://github.com/", "https://x-access-token:#{token}@github.com/") if token
+    super
   end
 end
 
