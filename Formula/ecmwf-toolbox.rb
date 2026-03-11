c445126e751875adcd61c7975fa9af665cc3ba70
Jakub Recman <jakub@oxidian.com>
2026-03-11 16:15:47 +0100

    Use GH API endpoint to fetch tarball

--- Formula/ecmwf-toolbox.rb
+++ Formula/ecmwf-toolbox.rb
@@ -1,7 +1,7 @@
 class EcmwfToolbox < Formula
   desc "ECMWF software bundle: ecCodes, Magics, Metview, Atlas, and more"
   homepage "https://github.com/recmanj/ecmwf-toolbox"
-  url "https://github.com/recmanj/ecmwf-toolbox/archive/refs/tags/2026.01.0.0.tar.gz", headers: ["Authorization: token #{ENV["HOMEBREW_ECMWF_TOOLBOX_TOKEN"]}"]
+  url "https://api.github.com/repos/recmanj/ecmwf-toolbox/tarball/2026.01.0.0", headers: ["Authorization: token #{ENV["HOMEBREW_ECMWF_TOOLBOX_TOKEN"]}"]
   sha256 "fe8b131c76b2b78c34f04a275a1e16d2b1ef29fa7c245a549ab45c5a5bc0aa9b"
   license "Apache-2.0"
 
